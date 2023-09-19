0becdd4fb54211cd6c8b0580e0ed8b00a0a4c08b
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2023-09-19 16:18:11 -0400

    init update

--- __init__.py
+++ __init__.py
@@ -1,14 +1,15 @@
 """
     A module that makes omp function call available in python
 """
+import os as _os, platform as _platform
+mingw_path = 'C:\\cygwin64\\usr\\x86_64-w64-mingw32\\sys-root\\mingw\\bin'
+if _os.name == 'nt':
+    if _platform.python_version_tuple()[0]== '2' : #python 2
+        if ( mingw_path not in _os.environ['PATH'] ) :
+            _os.environ['PATH'] = mingw_path+_os.path.pathsep+_os.environ['PATH']
+    else : # python 3      
+        _os.add_dll_directory("C:/cygwin64/usr/x86_64-w64-mingw32/sys-root/mingw/bin")
 
-import os as _os
-s = _os.path.abspath("C:/cygwin64/usr/x86_64-w64-mingw32/sys-root/mingw/bin")
-
-if _os.name == "nt" and s not in _os.environ["PATH"]:
-  _os.environ["PATH"] = s+";"+_os.environ["PATH"]
-  
 __all__ = ["omp_extra"]
 
-# REMOVING UNDESIRED NAME FROM NAME SPACE
-del s
+del mingw_path
