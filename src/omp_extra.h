114cb57640116bc74f6891a286c8d34855ece4c1
Simon Bolduc Beaudoin <Simon.Bolduc.Beaudoin@usherbrooke.ca>
2024-10-03 11:22:02 -0400

    Beautifying clang-format

--- src/omp_extra.h
+++ src/omp_extra.h
@@ -1,23 +1,23 @@
 #pragma once
 
 #if defined(__CYGWIN__) || defined(__MINGW64__)
-    // see number from: sdkddkver.h
-    // https://docs.microsoft.com/fr-fr/windows/desktop/WinProg/using-the-windows-headers
-    #define _WIN32_WINNT 0x0602 // Windows 8
-	#define WIN32_LEAN_AND_MEAN //Faster compilation (remove some unused API)
-	#include <windows.h>
-    #include <Processtopologyapi.h>
-    #include <processthreadsapi.h>
+// see number from: sdkddkver.h
+// https://docs.microsoft.com/fr-fr/windows/desktop/WinProg/using-the-windows-headers
+#define _WIN32_WINNT 0x0602 // Windows 8
+#define WIN32_LEAN_AND_MEAN // Faster compilation (remove some unused API)
+#include <windows.h>
+#include <Processtopologyapi.h>
+#include <processthreadsapi.h>
 #endif
 
-#include <stdio.h>
-#include <omp.h>
-#include<thread>
 #include <assert.h>
+#include <omp.h>
+#include <stdio.h>
+#include <thread>
 
 void manage_thread_affinity();
 
-template<class BinType>
-void reduce( BinType** arrs, uint64_t bins, uint64_t begin, uint64_t end);
+template <class BinType>
+void reduce(BinType **arrs, uint64_t bins, uint64_t begin, uint64_t end);
 
 int physical_n_threads();
--- src/python_submodule.cpp
+++ src/python_submodule.cpp
@@ -4,9 +4,8 @@
 // See Pybind11 FAQ «How can I reduce the build time ?» :
 // https://pybind11.readthedocs.io/en/stable/faq.html#how-can-i-reduce-the-build-time
 
-//Python Binding and Time_Quad class instances.
-PYBIND11_MODULE(omp_extra, m)
-{
-    m.doc() = "Some OpenMP functions to modify environnement variables\n";
-	init_omp_extra(m);
+// Python Binding and Time_Quad class instances.
+PYBIND11_MODULE(omp_extra, m) {
+  m.doc() = "Some OpenMP functions to modify environnement variables\n";
+  init_omp_extra(m);
 }
